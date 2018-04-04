a403609ee2bd4a05f82f9582fee2bae8b0ab1014
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    kminilte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,7 @@
 PRODUCT_MAKEFILES := \
-     device/samsung/kminilte/lineage_kminilte.mk 
-     
+     device/samsung/kminilte/lineage_kminilte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_kminilte-user \
+    lineage_kminilte-userdebug \
+    lineage_kminilte-eng
